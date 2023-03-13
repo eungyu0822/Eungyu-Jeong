d1951fd161d4bfe452183536520176e03ba6a1a7
Eungyu-Jeong <eungyu0822@naver.com>
2023-03-13 22:14:56 +0900

    Add Python File [hw1.py]

---
+++ hw1.py
@@ -0,0 +1,10 @@
+#202310972 정은규
+def get_radius(prompt):
+    r = int(input(prompt))
+    return r
+def get_circle_area():
+    print(3.14 * result * result)
+input_prompt = input('넓이를 구하고자 하는 원의 반지름은? ')
+result = get_radius(input_prompt)
+get_circle_area()
+
