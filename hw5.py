2e11b8db3da2f74584d09ec9324356549ab43c9b
Eungyu-Jeong <eungyu0822@naver.com>
2023-04-13 19:04:28 +0900

    Add Python File [hw5.py]

---
+++ hw5.py
@@ -0,0 +1,32 @@
+def read_single_digit(A,B,C) :
+    
+
+
+
+def read_number(T) :
+    if 100 <= T < 200 :
+        return 1
+    elif 200 <= T < 300 :
+        return 2
+    elif 300 <= T < 400 :
+        return 3
+    elif 400 <= T < 500 :
+        return 4
+    elif 500 <= T < 600 :
+        return 5
+    elif 600 <= T < 700 :
+        return 6
+    elif 700 <= T < 800 :
+        return 7
+    elif 800 <= T < 900 :
+        return 8
+    elif 900 <= T < 1000 :
+        return 9
+
+I = int(input('세 자리 정수 입력: '))
+A = read_number(I)
+
+
+
+
+
